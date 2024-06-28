516bf94ecbcbfd9c5cebf4e7ea7a233c5ed383fd
Alborz Babaie Mouziraji <alborz.babaie@gmail.com>
2024-06-28 19:39:02 +0200

    Add procedural solution

--- 001/Python/solution.py
+++ 001/Python/solution.py
@@ -1,2 +1,15 @@
+
+def resolve() -> int:
+
+    _sum: int = 0
+
+    for i in range(1, 1000):
+        if i % 3 == 0 or i % 5 == 0:
+            _sum += i
+
+    return _sum
+
 if __name__ == "__main__":
-    pass
+
+    print(resolve())
+
